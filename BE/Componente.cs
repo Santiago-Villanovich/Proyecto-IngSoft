553227bdf4ca2fcde62fae4e4bbb43d3b12e8de9
MatiasEnrique <mati.seba.enrique@gmail.com>
2023-05-20 20:15:42 -0300

    comienzo implementacion composite

---
+++ BE/Componente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public abstract class Componente
+    {
+        int _id;
+
+        string _nombre;
+
+        public Componente  (string nombre)
+        {
+            _nombre = nombre;
+            
+        }
+
+        public Componente(string nombre, int id)
+        {
+            _nombre = nombre;
+            _id = id;
+
+        }
+
+        public string Nombre { get { return _nombre; } }
+
+        public int Id { get { return _id; } }
+
+        public abstract void AgregarHijo(Componente permiso);
+
+        public abstract IList<Componente> ObtenerHijos();
+
+
+    }
+}
---
+++ BE/Permiso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Permiso : Componente
+    {
+        List<Componente> _hijos;
+
+        public Permiso(string nombre): base(nombre)
+        {
+            _hijos = new List<Componente>();
+        }
+
+        public override void AgregarHijo(Componente permiso)
+        {
+            _hijos.Add(permiso);
+        }
+
+        public override IList<Componente> ObtenerHijos()
+        {
+            return _hijos.ToArray();
+        }
+    }
+}
