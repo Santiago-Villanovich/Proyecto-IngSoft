4ae0246191d8489a89e98eeef0ceb3242994fac6
MatiasEnrique <mati.seba.enrique@gmail.com>
2023-05-28 18:22:54 -0300

    observer implementado, composite empezado

--- DAL/DAL_Bitacora.cs
+++ DAL/DAL_Bitacora.cs
@@ -1,11 +1,11 @@
-﻿using BE;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace DAL
 {
