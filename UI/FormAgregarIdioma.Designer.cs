993adfac5d1e00561923b687a0c459871c11f3ae
Santiago <santiagovillanovich392@gmail.com>
2023-06-23 20:35:44 -0300

    Cambios

--- UI/FormAgregarIdioma.Designer.cs
+++ UI/FormAgregarIdioma.Designer.cs
@@ -115,7 +115,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 536);
+            this.ClientSize = new System.Drawing.Size(377, 530);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormAgregarIdioma";
